bf308fedb4f4682d170d236fcc58d25bc5e42971
To Ngoc Anhs <ngocanhgidi@gmail.com>
2020-06-30 21:51:04 +0700

    fix bug cant access show recipe page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,12 +4,12 @@ class RecipesController < ApplicationController
   before_action :process_image, only: [:create]
 
   def show
-    if current_user != @recipe.user || @recipe.published
-      redirect_to root_path
-    else
+    if current_user == @recipe.user || @recipe.published
       # save log recipe was viewd by current user
       exist = ViewedRecipe.find_by(user_id: current_user.id, recipe_id: @recipe.id).present?
       ViewedRecipe.create(user_id: current_user.id, recipe_id: @recipe.id) unless exist
+    else
+      redirect_to root_path
     end
   end
   
