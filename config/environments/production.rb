ad054ff9edd91621b9bbe69265dc803df1937643
Ngoc Anhs <ngocanhgidi@gmail.com>
2020-07-02 22:52:51 +0700

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "cooksocial.herokuapp.com",
+    domain: "example.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
