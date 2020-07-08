6bac66d51d4a229b130130bade2bd8316b942428
Ngoc Anhs <ngocanhgidi@gmail.com>
2020-07-08 23:33:01 +0700

    similar recipe in detail recipe page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,6 +8,7 @@ class RecipesController < ApplicationController
       # save log recipe was viewd by current user
       exist = ViewedRecipe.find_by(user_id: current_user.id, recipe_id: @recipe.id).present?
       ViewedRecipe.create(user_id: current_user.id, recipe_id: @recipe.id) unless exist
+      @similar_recipes = Recipe.search(@recipe.title).drop(1).take(10).shuffle.take(4)
     else
       redirect_to root_path
     end
