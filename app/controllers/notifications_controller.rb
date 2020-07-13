68c418913515534e51a50cb2ab45f7109764a54f
Ngoc Anhs <ngocanhgidi@gmail.com>
2020-07-13 23:09:18 +0700

    en/vn v2

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.notifications.limit(5).order("created_at desc")
+    @notifications = current_user.notifications.limit(5).order(created_at: :desc)
   end
 
   def mark_as_read
